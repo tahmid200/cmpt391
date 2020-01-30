3d2245fa408b470954af936305931eaf64bf7aa6
Ben Ha <benha0898@gmail.com>
2020-01-30 13:51:23 -0700

    create project

---
+++ CMPT391V2/CMPT391V2/Database/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].student
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [last_name] NCHAR(20) NULL, 
+    [first_name] NCHAR(20) NULL, 
+    [dob] DATE NULL, 
+    [street] NCHAR(20) NULL, 
+    [city] NCHAR(20) NULL, 
+    [province] NCHAR(10) NULL, 
+    [country] NCHAR(20) NULL, 
+    [postal] NCHAR(10) NULL, 
+    [total_credits] NCHAR(10) NULL,
+)
