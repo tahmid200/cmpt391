d01326170dd04ac28ceb15c2445b0ed53e27c268
Ben Ha <benha0898@gmail.com>
2020-01-30 14:11:59 -0700

    Made changes

--- CMPT391V2/CMPT391V2/Database/dbo.Table.sql
+++ CMPT391V2/CMPT391V2/Database/dbo.Table.sql
@@ -1,13 +1,8 @@
-﻿CREATE TABLE [dbo].student
+﻿CREATE TABLE [dbo].department
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [last_name] NCHAR(20) NULL, 
-    [first_name] NCHAR(20) NULL, 
-    [dob] DATE NULL, 
-    [street] NCHAR(20) NULL, 
-    [city] NCHAR(20) NULL, 
-    [province] NCHAR(10) NULL, 
-    [country] NCHAR(20) NULL, 
-    [postal] NCHAR(10) NULL, 
-    [total_credits] NCHAR(10) NULL,
+	id INT NOT NULL PRIMARY KEY,
+	dept_name NCHAR(20) UNIQUE NULL,
+	building INT NULL,
+	dept_head INT UNIQUE null,
+	CONSTRAINT fk_department_instructor FOREIGN KEY (dept_head) REFERENCES instructor (id),
 )
